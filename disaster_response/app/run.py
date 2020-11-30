489b90647270282b7940412276ba55acd7bdc2e0
Jasmit Singh <jsingh@perforce.com>
2020-11-29 21:05:43 -0700

    Added front-end for disaster message classification - Added visuals for data using plotly.  Currently, displaying genres of   disaster messages as bar plot - users can input messages to be classified into multiple categories - Removed code from custom_transformer into train_classifier.  Better while   pickling - Updated run.py to perform initial data read, plots, classification

---
+++ disaster_response/app/run.py
@@ -0,0 +1,131 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+from sklearn.base import BaseEstimator, TransformerMixin
+
+app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        #print("text:{}".format(text))
+        pos_tags = nltk.pos_tag(tokenize(text))
+        if len(pos_tags) == 0:
+            return False
+        first_word, first_tag = pos_tags[0]
+        if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+            return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+def tokenize(text):
+    '''
+    :param text: document to be tokenized
+    :return: array of tokens where the original document is reduced by removing punctuation, stop words, lemmatized
+    '''
+    # convert to lowercase
+    text = text.lower()
+    # remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    # tokenize text
+    tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok.strip())
+        clean_tokens.append(clean_tok)
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
+
+# load model
+model = joblib.load("../models/disaster_classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
--- disaster_response/models/train_classifier.py
+++ disaster_response/models/train_classifier.py
@@ -19,8 +19,57 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import make_scorer, accuracy_score, classification_report
-from custom_transformer import tokenize
-from custom_transformer import StartingVerbExtractor
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        #print("text:{}".format(text))
+        pos_tags = nltk.pos_tag(tokenize(text))
+        if len(pos_tags) == 0:
+            return False
+        first_word, first_tag = pos_tags[0]
+        if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+            return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+class TextLengthExtractor(BaseEstimator, TransformerMixin):
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(len)
+        return pd.DataFrame(X_tagged)
+
+def tokenize(text):
+    '''
+    :param text: document to be tokenized
+    :return: array of tokens where the original document is reduced by removing punctuation, stop words, lemmatized
+    '''
+    # convert to lowercase
+    text = text.lower()
+    # remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    # tokenize text
+    tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok.strip())
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 def load_data(database_filepath):
     '''
 
@@ -65,7 +114,6 @@ def build_tuned_model():
        :return: Model to be used for learning
        '''
     from sklearn.pipeline import FeatureUnion
-    from custom_transformer import TextLengthExtractor
     pipeline = Pipeline([
         ('features', FeatureUnion([
 
